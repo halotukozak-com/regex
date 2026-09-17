da5230df38fbdd3ab79b2eac5274eb6c6d0d57b8
halotukozak <halotukozak@gmail.com>
2026-09-17 12:58:12 +0000

    🔄 synced local '.mima/bin-compat-check.scala' with remote '.mima/bin-compat-check.scala'

--- .mima/bin-compat-check.scala
+++ .mima/bin-compat-check.scala
@@ -1,5 +1,5 @@
 //> using scala 2.13
-//> using dep com.typesafe::mima-core:1.1.6
+//> using dep com.typesafe::mima-core:1.2.0
 
 // Binary-compatibility check between a released artifact and the current
 // build, via MiMa's core API (there is no scala-cli MiMa plugin).
